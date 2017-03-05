5ff2ed10e094a2a20b5bf7e5e65039625a6088fc
Stefan Aerts <stefanaerts@Stefans-MacBook-Pro.local>
2017-03-05 20:45:49 +0100

    jaja

--- appendUserandDateToIndex.sh
+++ appendUserandDateToIndex.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 whoami >> ./src/index.html
-echo ' Continuous Integration build on ' >> ./src/index.html
+echo ' Continuous Integration build on' >> ./src/index.html
 date >> ./src/index.html
