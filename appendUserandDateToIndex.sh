cb59ce07cb9d3c0bedb4eafc13796208f7b54363
Stefan Aerts <stefanaerts@Stefans-MacBook-Pro.local>
2017-03-06 19:00:11 +0100

    changed git log output

--- appendUserandDateToIndex.sh
+++ appendUserandDateToIndex.sh
@@ -3,4 +3,5 @@
 whoami >> ./src/index.html
 echo ' CI build and push to firebase on' >> ./src/index.html
 zdump CET >> ./src/index.html
-git log HEAD -1 > ./src/assets/git-latest-commit.txt
+git log --pretty=format:"%h - %an, %ar : %s" -1 --graph > ./src/assets/git-latest-commit.txt
+# git log HEAD -1 > ./src/assets/git-latest-commit.txt
