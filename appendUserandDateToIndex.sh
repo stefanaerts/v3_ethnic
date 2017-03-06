5d1031897a80a4b2dbb1d9f6a6702cc56c2d8301
Stefan Aerts <stefanaerts@Stefans-MacBook-Pro.local>
2017-03-06 19:21:56 +0100

    changed appensh

--- appendUserandDateToIndex.sh
+++ appendUserandDateToIndex.sh
@@ -3,5 +3,7 @@
 whoami >> ./src/index.html
 echo ' CI build and push to firebase on' >> ./src/index.html
 zdump CET >> ./src/index.html
+rm ./src/assets/git-latest-commit.txt
+touch ./src/assets/git-latest-commit.txt
 git log --pretty=format:"%h - %an, %ar : %s" -1 --graph > ./src/assets/git-latest-commit.txt
 # git log HEAD -1 > ./src/assets/git-latest-commit.txt
