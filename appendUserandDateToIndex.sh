3e60ce272f0f47639fc20a7888ba0f2c7a1de949
Stefan Aerts <stefanaerts@Stefans-MacBook-Pro.local>
2017-03-06 20:44:25 +0100

    append at bottom onf index.html

--- appendUserandDateToIndex.sh
+++ appendUserandDateToIndex.sh
@@ -3,7 +3,9 @@
 whoami >> ./src/index.html
 echo ' CI build and push to firebase on' >> ./src/index.html
 zdump CET >> ./src/index.html
-rm ./src/assets/git-latest-commit.txt
-touch ./src/assets/git-latest-commit.txt
-git log --pretty=format:"%h - %an, %ad : %s" --date=format:'%Y-%m-%d %H:%M:%S' -1 --graph > ./src/assets/git-latest-commit.txt
-# git log HEAD -1 > ./src/assets/git-latest-commit.txt
+# rm ./src/assets/git-latest-commit.txt
+# touch ./src/assets/git-latest-commit.txt
+# git log --pretty=format:"%h - %an, %ad : %s" --date=format:'%Y-%m-%d %H:%M:%S' -1 --graph > ./src/assets/git-latest-commit.txt
+
+git log --pretty=format:"%h - %an, %ad : %s" --date=format:'%Y-%m-%d %H:%M:%S' -1 --graph >> ./src/index.html
+
